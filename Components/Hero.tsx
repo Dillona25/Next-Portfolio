18f2e7b92a676831b838e84ca4218f6525065885
Dillona25 <Dillyb00by49@gmail.com>
2024-05-19 01:09:51 -0500

    Add: Final Hero section comeplete

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,13 +1,13 @@
 import { Button } from "./ui/Button";
 import { TextGenerateEffect } from "./ui/TextGenerate";
 import { FaLocationArrow } from "react-icons/fa6";
-import Portrait from "../public/95C533F6-6153-4D43-ACF5-850A5ED80326_1_102_a.jpeg";
+import Portrait from "../public/Portrait.jpeg";
 
 export const Hero = () => {
   return (
-    <div className="flex m-auto justify-center items-center h-screen">
+    <div className="flex flex-col lg:flex-row gap-20 justify-center items-center h-screen mt-40 mb-10 lg:mt-0 lg:mb-0">
       <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.02] bg-grid-black-100/[0.2]
+        className="h-full md:h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.02] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
         <div
@@ -15,32 +15,30 @@ export const Hero = () => {
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
-      <div className="relative z-10">
-        <div className="flex items-center justify-center">
-          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-            <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-              Created using Next.js & Aceternity UI
-            </p>
-            <TextGenerateEffect words="Transforming Concepts into Seamless User Experiences" />
-            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-              Hi 👋 Im Dillon, a full stack Web Developer
-            </p>
+      <div className="flex flex-col items-center justify-center relative z-10">
+        <div className="max-w-[900px]">
+          <p className="uppercase tracking-widest text-xs m-auto text-blue-100 max-w-80">
+            Created with Next.js & Aceternity UI
+          </p>
+          <TextGenerateEffect words="Transforming Concepts into Seamless User Experiences" />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi 👋 Im Dillon, a full stack Web Developer
+          </p>
+          <a href="#projects">
             <Button
-              otherClasses="backdrop-blur-lg"
+              otherClasses="backdrop-blur-lg mx-auto"
               title="Explore more"
               icon={<FaLocationArrow />}
               position="right"
             />
-          </div>
-          <div>
-            <img
-              alt="portrait"
-              src={Portrait.src}
-              className="h-[600px] w-fit rounded-2xl"
-            />
-          </div>
+          </a>
         </div>
       </div>
+      <img
+        alt="portrait"
+        src={Portrait.src}
+        className="h-[350px] md:h-[450px] xl:h-[600px] rounded-xl relative"
+      />
     </div>
   );
 };
--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -5,8 +5,8 @@ import { Button } from "./ui/Button";
 
 export const Projects = () => {
   return (
-    <div className="relative flex flex-col gap-10 pb-20">
-      <h1 className="text-left font-bold text-[40px] md:text-5xl lg:text-6xl border-b-[1px] border-[#2b2b2b] pb-10">
+    <div className="relative flex flex-col gap-10 pb-5 sm:pb-20" id="projects">
+      <h1 className="text-left font-bold text-[40px] md:text-5xl lg:text-6xl border-b-[1px] border-white sm:border-[#2b2b2b] pb-5 sm:pb-10">
         Top projects
       </h1>
       <div className="flex gap-10 flex-wrap ">
