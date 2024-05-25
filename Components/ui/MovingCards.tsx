b6e5fd6f9708934302d3be8c99733e951ba4f1bb
Dillona25 <Dillyb00by49@gmail.com>
2024-05-24 23:04:59 -0500

    Fix: fixed another deployment bug

--- Components/ui/MovingCards.tsx
+++ Components/ui/MovingCards.tsx
@@ -63,7 +63,7 @@ export const InfiniteMovingCards = ({
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "10s");
+        containerRef.current.style.setProperty("--animation-duration", "1s");
       } else if (speed === "normal") {
         containerRef.current.style.setProperty("--animation-duration", "40s");
       } else {
