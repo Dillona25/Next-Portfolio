da26c779876017dfc131f2b9c60865c317a6afed
Dillona25 <Dillyb00by49@gmail.com>
2024-07-20 18:38:25 -0500

    Updated UI and about section

--- Components/Testimonials.tsx
+++ Components/Testimonials.tsx
@@ -8,10 +8,10 @@ export const Testimonials = ({ className }: { className?: ReactNode }) => {
     <section id="test" className={`relative bg-black-200 ${className}`}>
       <div className="max-w-[1350px] m-auto px-7">
         <div className="flex gap-4 items-center sm:pb-10">
+          <span className="border-[#797979] ml-2 border-[1px] w-full"></span>
           <h1 className="text-left font-bold text-[40px] leading-[40px] md:text-5xl lg:text-6xl whitespace-nowrap">
             Testimonials<span className="text-purple">.</span>
           </h1>
-          <span className="border-[#797979] ml-2 border-[1px] w-full"></span>
         </div>
         <TestimonialCard items={testimonials} />
       </div>
